01f0ca3f577e52c721a8f348c2dbae5c217f4390
Cyril Gourdon <cyril.gourdon.02@gmail.com>
2021-12-21 13:59:02 +0100

    correction script js

--- souris.js
+++ souris.js
@@ -1,8 +1,8 @@
 let balls = document.getElementsByClassName('ball');
 
 document.onmousemove = function () {
-    let x  = event.clientX * 100 / window.innerWidth + "%";
-    let y = event.clientY * 100 / window.innerWidth + "%";
+    var x  = event.clientX * 100 / window.innerWidth + "%";
+    var y = event.clientY * 100 / window.innerHeight + "%";
 
     for (let i = 0; i < 2; i++) {
         balls[i].style.left = x;
