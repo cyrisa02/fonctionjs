60c4816b2cd80555aea66c9acb7e4abc69724fd1
Cyril Gourdon <cyril.gourdon.02@gmail.com>
2022-01-04 13:17:38 +0100

    date en js

---
+++ calendrier/date.js
@@ -0,0 +1,13 @@
+function datesInterval(begin, end, test) {
+  console.log(begin.getTime());
+  const isBetween = test > begin && test < end;
+  return isBetween;
+}
+
+console.log(
+  datesInterval(
+    new Date(2007, 05, 30), // depuis le 1er janv 1970 en seconde
+    new Date(2007, 06, 30),
+    new Date(2007, 06, 15)
+  )
+);
